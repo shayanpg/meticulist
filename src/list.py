b48e9182d2a26717feec4a6ed798455ab06c9275
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2020-06-07 22:09:53 -0700

    playing with unit tests

---
+++ src/list.py
@@ -0,0 +1,6 @@
+def to_test():
+    return 7
+
+
+class Testing:
+    pass
--- test/test_all.py
+++ test/test_all.py
@@ -1,9 +1,11 @@
 import unittest
+from src import list
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
+        self.assertEqual(list.to_test(), 7)
 
 
 if __name__ == '__main__':
